3b247e694cfa4037fd7f78e156f605f38955a61e
chandima <chandimar.online@gmail.com>
2024-06-13 19:47:47 +0530

    feat: add clerk logout

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import {Source_Sans_3} from "next/font/google";
+import React from "react";
+import {ClerkProvider} from "@clerk/nextjs";
+import {Toaster} from "@/components/ui/sonner";
+
+const source_sans_3 = Source_Sans_3({ subsets: ["latin"] });
+
+export const metadata = {
+  title: 'Magic Apply',
+  icons: {
+    icon: '/images/logo.svg'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    <ClerkProvider>
+      <body className={source_sans_3.className}>
+      <Toaster />
+      {children}</body>
+    </ClerkProvider>
+    </html>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-   <main>Magic Apply</main>
-  );
-}
