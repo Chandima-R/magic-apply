254466eab66c61b2dfc9a3405f6bfefca3f89b3a
chandima <chandimar.online@gmail.com>
2024-06-28 16:20:49 +0530

    feat: add dashboard new screen

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,10 +1,25 @@
-'use client'
+"use client";
 import { Dashboard } from "@/modules/dashboard/components/dashboard";
+import { CustomButtonCard } from "@/modules/shared/components/custom-button-card";
+import { Aperture, CircleUser, Link, Magnet } from "lucide-react";
 
 export default function page() {
-    return (
-        <main className={'max-w-[1440px] w-full mx-auto p-8'}>
-            <Dashboard />
-        </main>
-    )
+  return (
+    <main className={"max-w-[1440px] w-full mx-auto p-8"}>
+      <div className="flex items-center gap-8">
+        <CustomButtonCard
+          icon={CircleUser}
+          title="your profile"
+          link="master-resume"
+        />
+        <CustomButtonCard
+          icon={Aperture}
+          title="your master resume"
+          link="profile"
+        />
+        <CustomButtonCard icon={Link} title="apply jobs" link="apply-jobs" />
+      </div>
+      {/* <Dashboard /> */}
+    </main>
+  );
 }
---
+++ src/modules/shared/components/custom-button-card.tsx
@@ -0,0 +1,21 @@
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  link: string;
+  icon: LucideIcon;
+}
+
+export const CustomButtonCard = ({ title, link, icon }: Props) => {
+  const Icon = icon;
+  return (
+    <Link
+      href={`/${link}`}
+      className="border h-44 w-36 rounded border-dashed p-4 flex items-center flex-col justify-center hover:shadow cursor-pointer duration-150"
+    >
+      <Icon className="size-6 text-honoluluBlue" />
+      <p className="capitalize font-normal text-xl text-center">{title}</p>
+    </Link>
+  );
+};
