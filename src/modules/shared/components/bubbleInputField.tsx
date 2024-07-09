0c270821c8bdf0969a6c85ad66da8f21fe83738a
chandima <chandimar.online@gmail.com>
2024-07-09 13:25:40 +0530

    feat: add additional fields to skills section

---
+++ src/modules/shared/components/bubbleInputField.tsx
@@ -0,0 +1,70 @@
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { X } from "lucide-react";
+import { ReactNode, JSXElementConstructor, ReactElement, ReactPortal } from "react";
+
+interface Props {
+    fieldLabel: string;
+    fieldName: string;
+    placeholder: string;
+    inputValue: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onRemove: (index: number) => void;
+    control: any;
+    options: (string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<ReactNode | void> | null | undefined)[];
+}
+
+export const BubbleInputField = ({
+                                     fieldLabel,
+                                     fieldName,
+                                     placeholder,
+                                     control,
+                                     options,
+                                     inputValue,
+                                     onChange,
+                                     onRemove,
+                                 }: Props) => {
+    return (
+        <FormField
+            control={control}
+            name={fieldName}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>{fieldLabel}</FormLabel>
+                    <FormControl>
+                        <Input
+                            placeholder={placeholder}
+                            {...field}
+                            value={inputValue}
+                            onChange={onChange}
+                        />
+                    </FormControl>
+                    <div className="flex flex-wrap gap-2 mt-2">
+                        {options.map((option: any, index) => (
+                            <div
+                                key={index}
+                                className="text-sm items-center flex cursor-pointer font-normal rounded-full capitalize text-black bg-slate-200/40 px-4 py-1.5 hover:bg-slate-400/40 shadow-sm"
+                            >
+                                <span>{option}</span>
+                                <button
+                                    type="button"
+                                    onClick={() => onRemove(index)}
+                                    className="ml-2 text-red-400"
+                                >
+                                    <X className="size-4" />
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+};
