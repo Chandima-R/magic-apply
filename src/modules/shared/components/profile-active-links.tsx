d83e8bc14d2d9cfd74099ccd526600e2ae6b5cb8
chandima <chandimar.online@gmail.com>
2024-09-25 21:25:24 +0530

    feat: update required fields colors in profile section

--- src/modules/shared/components/profile-active-links.tsx
+++ src/modules/shared/components/profile-active-links.tsx
@@ -1,29 +1,30 @@
-import { Button } from "@/components/ui/button";
-import { profileLinks } from "@/modules/shared/utils/profile-links";
+import {Button} from "@/components/ui/button";
+import {profileLinks} from "@/modules/shared/utils/profile-links";
 import Link from "next/link";
 
 interface Props {
-  activeLink: string;
+    activeLink: string;
 }
 
-export const ProfileActiveLinks = ({ activeLink }: Props) => {
-  return (
-    <div className="flex flex-wrap gap-6 mb-10 w-full justify-start ">
-      {profileLinks.map((link) => (
-        <Link href={link.href} key={link.href}>
-          <Button
-            size={"sm"}
-            key={link.href}
-            className={`text-sm cursor-pointer font-normal rounded-md capitalize text-black bg-slate-200/40 px-2 py-1 ${
-              link.href.split("/")[2] === activeLink
-                ? "text-white bg-honoluluBlue"
-                : ""
-            } hover:bg-slate-400/40 transition duration-100`}
-          >
-            {link.label}
-          </Button>
-        </Link>
-      ))}
-    </div>
-  );
+export const ProfileActiveLinks = ({activeLink}: Props) => {
+
+    return (
+        <div className="flex flex-wrap gap-6 mb-10 w-full justify-start ">
+            {profileLinks.map((link) => (
+                <Link href={link.href} key={link.href}>
+                    <Button
+                        size={"sm"}
+                        key={link.href}
+                        className={`text-sm cursor-pointer font-normal rounded-md capitalize text-black px-2 py-1 ${link?.required === true ? 'bg-[#c9f0f8]/40' : 'bg-slate-200/40'} ${
+                            link.href.split("/")[2] === activeLink
+                                ? "text-white bg-honoluluBlue"
+                                : ""
+                        } hover:bg-slate-400/40 transition duration-100`}
+                    >
+                        {link.label}
+                    </Button>
+                </Link>
+            ))}
+        </div>
+    );
 };
--- src/modules/shared/utils/profile-links.ts
+++ src/modules/shared/utils/profile-links.ts
@@ -2,38 +2,47 @@ export const profileLinks = [
     {
         href: "/profile/contact",
         label: "contact",
+        required: true
     },
     {
         href: "/profile/experience",
         label: "experience",
+        required: true
     },
     {
         href: "/profile/education",
         label: "education",
+        required: true
     },
     {
         href: "/profile/projects",
         label: "projects",
+        required: true
     },
     {
         href: "/profile/skills",
         label: "skills and languages",
+        required: true
     },
     {
         href: "/profile/summary",
         label: "summary",
+        required: true
     },
     {
         href: "/profile/certifications",
         label: "certifications",
+        required: false
     },
     {
         href: "/profile/coursework",
         label: "coursework",
+        required: false
     },
     {
         href: "/profile/involvements",
         label: "involvements",
+        required: false
     },
 
     {
