7e3373e4f9f8b6726e29f9c8599b9a0c2ee2c1a7
chandima <chandimar.online@gmail.com>
2024-10-23 19:46:09 +0530

    feat: update projects endpoint

--- src/graphql/project/index.ts
+++ src/graphql/project/index.ts
@@ -2,7 +2,10 @@ import { gql } from "@apollo/client";
 
 export const PROJECT_INFORMATION_BY_USER_ID = gql`
   subscription projectInformation($_eq: String) {
-    project(where: { user_id: { _eq: $_eq } }, order_by: { project_end_date: desc }) {
+    project(
+      where: { user_id: { _eq: $_eq } }
+      order_by: { project_end_date: desc }
+    ) {
       id
       project_end_date
       project_name
@@ -13,6 +16,7 @@ export const PROJECT_INFORMATION_BY_USER_ID = gql`
       project_url
       user_id
       visibility
+      isCurrent
     }
   }
 `;
@@ -30,6 +34,7 @@ export const VIEW_PROJECT_BY_ID = gql`
       project_url
       user_id
       visibility
+      isCurrent
     }
   }
 `;
@@ -44,6 +49,7 @@ export const ADD_NEW_PROJECT_BY_USER_ID = gql`
     $project_location: String
     $project_name: String
     $project_end_date: String
+    $isCurrent: Boolean
   ) {
     insert_project(
       objects: {
@@ -55,6 +61,7 @@ export const ADD_NEW_PROJECT_BY_USER_ID = gql`
         project_location: $project_location
         project_name: $project_name
         project_end_date: $project_end_date
+        isCurrent: $isCurrent
       }
     ) {
       affected_rows
@@ -72,6 +79,7 @@ export const UPDATE_PROJECT_BY_ID = gql`
     $project_start_date: String
     $project_url: String
     $_eq: uuid
+    $isCurrent: Boolean
   ) {
     update_project(
       where: { id: { _eq: $_eq } }
@@ -83,6 +91,7 @@ export const UPDATE_PROJECT_BY_ID = gql`
         project_role_description: $project_role_description
         project_start_date: $project_start_date
         project_url: $project_url
+        isCurrent: $isCurrent
       }
     ) {
       affected_rows
@@ -103,6 +112,7 @@ export const DELETE_PROJECT_BY_PK = gql`
       project_url
       user_id
       visibility
+      isCurrent
     }
   }
 `;
