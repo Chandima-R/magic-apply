8dc042b7b3894f1694254655165d6c052052d842
chandima <chandimar.online@gmail.com>
2024-10-28 18:08:34 +0530

    feat: add delete row

--- src/graphql/apply-jobs/index.ts
+++ src/graphql/apply-jobs/index.ts
@@ -4,21 +4,18 @@ export const APPLY_JOBS_INFORMATION_BY_USER_ID = gql`
   subscription applyJobsInformationByUserId($_eq: String) {
     apply_jobs(where: { user_id: { _eq: $_eq } }) {
       user_id
-      job_description
-      cover_letter
-      id
-      company_description
-      additional_question_one
-      additional_question_two
-      additional_question_three
       additional_information
+      additional_question_one
+      additional_question_three
+      additional_question_two
+      company_description
+      cover_letter
       custom_input
-      file_upload
       custom_text
+      file_upload
+      id
+      job_description
       master_resume
-      cover_letters {
-        letter
-      }
     }
   }
 `;
--- src/modules/apply-jobs/components/apply-jobs.tsx
+++ src/modules/apply-jobs/components/apply-jobs.tsx
@@ -12,12 +12,20 @@ import { CheckboxField } from "@/modules/shared/components/checkbox-input";
 import { TextArea } from "@/modules/shared/components/text-area";
 import { SingleFileDropBox } from "@/modules/shared/components/dropbox";
 import { PlusCircle, Send, Trash2 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { useUser } from "@clerk/nextjs";
 import { useMutation, useSubscription } from "@apollo/client";
-import { ADD_NEW_APPLY_JOBS_ROW_BY_USER_ID } from "@/graphql/apply-jobs";
+import {
+  ADD_NEW_APPLY_JOBS_ROW_BY_USER_ID,
+  APPLY_JOBS_INFORMATION_BY_USER_ID,
+  DELETE_APPLY_JOBS_ROW_BY_PK,
+  UPDATE_APPLY_JOBS_ROW_BY_USER_ID,
+} from "@/graphql/apply-jobs";
 import { CONTACT_INFORMATION } from "@/graphql/contact";
+import { LoadingSpinner } from "@/modules/shared/components/loading-spinner";
+import { PlanOverlay } from "./plan-overlay";
+import { GET_USER } from "@/graphql/user";
 
 const groupSchema = z
   .object({
@@ -88,9 +96,52 @@ export const ApplyJobs = () => {
   });
 
   const [addApplyJobs] = useMutation(ADD_NEW_APPLY_JOBS_ROW_BY_USER_ID);
+  const [updateApplyJobs] = useMutation(UPDATE_APPLY_JOBS_ROW_BY_USER_ID);
 
-  const { data: contactData, loading: contactLoading } = useSubscription(
-    CONTACT_INFORMATION,
+  const [deleteApplyJobsRow] = useMutation(DELETE_APPLY_JOBS_ROW_BY_PK);
+
+  const deleteApplyJobsRowAction = async (id: string) => {
+    try {
+      await deleteApplyJobsRow({
+        variables: {
+          _eq: id, // Ensure this matches the mutation's requirement
+        },
+      });
+      toast({
+        variant: "default",
+        title: "Success.",
+        description: "Row deleted successfully.",
+      });
+
+      // Refresh form state after successful deletion
+      const updatedFields = form
+        .getValues("groups")
+        .filter((_, index) => fields[index].id !== id);
+      form.reset({ groups: updatedFields });
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "There was an error deleting the row.",
+      });
+    }
+  };
+
+  const handleDeleteItem = (index: any) => {
+    remove(index);
+  };
+
+  const handleDelete = async (index: any) => {
+    const item = fields[index];
+    if (item.id) {
+      await deleteApplyJobsRowAction(item.id);
+    } else {
+      handleDeleteItem(index);
+    }
+  };
+
+  const { data: jobsData, loading: jobsLoading } = useSubscription(
+    APPLY_JOBS_INFORMATION_BY_USER_ID,
     {
       variables: {
         _eq: user?.id,
@@ -98,12 +149,29 @@ export const ApplyJobs = () => {
     }
   );
 
-  const signedUserData = contactData?.contact[0];
+  useEffect(() => {
+    if (jobsData && jobsData.apply_jobs.length > 0) {
+      const formattedData = jobsData.apply_jobs.map((job: any) => ({
+        id: job.id,
+        jobDescription: job.job_description,
+        customInput: job.custom_input,
+        masterResume: job.master_resume,
+        fileUpload: job.file_upload,
+        customText: job.custom_text,
+        companyDescription: job.company_description,
+        additionalInformation: job.additional_information,
+        additionalQuestion1: job.additional_question_one,
+        additionalQuestion2: job.additional_question_two,
+        additionalQuestion3: job.additional_question_three,
+        coverLetter: job.cover_letter,
+      }));
+
+      form.reset({ groups: formattedData });
+    }
+  }, [jobsData, form.reset]);
 
   async function onSectionSubmit(index: number) {
     const sectionData = form.getValues(`groups.${index}`);
-
-    console.log(12, sectionData);
     try {
       setIsLoading(true);
 
@@ -144,169 +212,184 @@ export const ApplyJobs = () => {
     }
   }
 
+  const { data: userData, loading: userLoading } = useSubscription(GET_USER);
+
+  const activeUser = userData?.user?.find(
+    (existingUser: any) => existingUser.user_clerk_id === user?.id
+  );
+
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(() => {})}>
-          {fields.map((field, index) => {
-            const watchedMasterResume = form.watch(
-              `groups.${index}.customInput`
-            );
-            return (
-              <div key={field.id}>
-                <div className="grid grid-cols-4 gap-4 items-start border rounded-md p-4 mb-4 bg-gray-50">
-                  <TextInput
-                    fieldLabel="Job Description (Paste or Link)"
-                    fieldName={`groups.${index}.jobDescription`}
-                    control={form.control}
-                    placeholder="Paste the JD (or link of JD here)"
-                    required
-                  />
+    <>
+      {activeUser?.user_plan.toLowerCase() === "free" && <PlanOverlay />}
+      {jobsLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(() => {})}>
+              {fields.map((field, index) => {
+                const watchedMasterResume = form.watch(
+                  `groups.${index}.customInput`
+                );
+                return (
+                  <div key={field.id}>
+                    <div className="grid grid-cols-4 gap-4 items-start border rounded-md p-4 mb-4 bg-gray-50">
+                      <TextInput
+                        fieldLabel="Job Description (Paste or Link)"
+                        fieldName={`groups.${index}.jobDescription`}
+                        control={form.control}
+                        placeholder="Paste the JD (or link of JD here)"
+                        required
+                      />
 
-                  <div>
-                    <SelectInput
-                      fieldName={`groups.${index}.customInput`}
-                      fieldLabel="Master Resume Or Manual (Input or Paste)"
-                      placeholder="Select your choice"
-                      control={form.control}
-                      options={
-                        resumeSelection?.map((opt: any) => ({
-                          label: opt.label,
-                          value: opt.value,
-                        })) || []
-                      }
-                    />
-
-                    <div className="mt-4">
-                      {watchedMasterResume === "masterResume" && (
-                        <CheckboxField
-                          fieldLabel="Use current master resume"
-                          fieldName={`groups.${index}.masterResume`}
+                      <div>
+                        <SelectInput
+                          fieldName={`groups.${index}.customInput`}
+                          fieldLabel="Master Resume Or Manual (Input or Paste)"
+                          placeholder="Select your choice"
                           control={form.control}
-                        />
-                      )}
-
-                      {watchedMasterResume === "customText" && (
-                        <TextArea
-                          fieldLabel="Add a custom description here"
-                          fieldName={`groups.${index}.customText`}
-                          control={form.control}
-                          placeholder="Add a custom description here.."
-                          required
-                        />
-                      )}
-
-                      {watchedMasterResume === "fileUpload" && (
-                        <SingleFileDropBox
-                          fieldLabel="Upload your resume here"
-                          fieldName={`groups.${index}.fileUpload`}
-                          control={form.control}
-                          required
-                          setValue={form.setValue}
-                          setUploading={() => {}}
-                        />
-                      )}
-                    </div>
-                  </div>
-
-                  <TextInput
-                    fieldLabel="Description of the Company"
-                    fieldName={`groups.${index}.companyDescription`}
-                    control={form.control}
-                    placeholder="Describe in few sentences or paste the link of home page"
-                    required
-                  />
-                  <TextInput
-                    fieldLabel="Additional Information (Highlight any recent projects or experience)"
-                    fieldName={`groups.${index}.additionalInformation`}
-                    control={form.control}
-                    placeholder="Describe if any"
-                  />
-                  <TextInput
-                    fieldLabel="Additional Questions Asked 1"
-                    fieldName={`groups.${index}.additionalQuestion1`}
-                    control={form.control}
-                    placeholder="Write / Paste the Q here"
-                  />
-                  <TextInput
-                    fieldLabel="Additional Questions Asked 2"
-                    fieldName={`groups.${index}.additionalQuestion2`}
-                    control={form.control}
-                    placeholder="Write / Paste the Q here"
-                  />
-                  <TextInput
-                    fieldLabel="Additional Questions Asked 3"
-                    fieldName={`groups.${index}.additionalQuestion3`}
-                    control={form.control}
-                    placeholder="Write / Paste the Q here"
-                  />
-                  <div className="flex items-center pt-4">
-                    <CheckboxField
-                      fieldLabel="Cover Letter Needed"
-                      fieldName={`groups.${index}.coverLetter`}
-                      control={form.control}
-                    />
-                  </div>
-
-                  <div className="col-span-4 mb-4">
-                    <div className="flex items-center justify-end w-full gap-4">
-                      <Button
-                        type="submit"
-                        size="sm"
-                        className="bg-honoluluBlue hover:bg-federalBlue text-white hover:text-white border-federalBlue"
-                        onClick={() => onSectionSubmit(index)}
-                      >
-                        <Send className="size-4 mr-2" />
-                        Generate
-                      </Button>
-
-                      {index === fields.length - 1 && (
-                        <Button
-                          type="button"
-                          onClick={() =>
-                            append({
-                              jobDescription: "",
-                              companyDescription: "",
-                              customInput: "masterResume",
-                              masterResume: false,
-                              fileUpload: "",
-                              customText: "",
-                              additionalInformation: "",
-                              additionalQuestion1: "",
-                              additionalQuestion2: "",
-                              additionalQuestion3: "",
-                              coverLetter: false,
-                            })
+                          options={
+                            resumeSelection?.map((opt: any) => ({
+                              label: opt.label,
+                              value: opt.value,
+                            })) || []
                           }
-                          className="bg-blue-700 hover:bg-blue-800 text-white hover:text-white border-blue-700"
-                          size="sm"
-                          variant={"outline"}
-                        >
-                          <PlusCircle className="size-4 mr-2" />
-                          Add
-                        </Button>
-                      )}
+                        />
 
-                      {fields.length > 1 && (
-                        <Button
-                          type="button"
-                          onClick={() => remove(index)}
-                          className="bg-red-500 text-white hover:bg-red-600 border-red-600 hover:text-white"
-                          size="sm"
-                          variant={"outline"}
-                        >
-                          <Trash2 className="size-4 mr-2" />
-                          Remove
-                        </Button>
-                      )}
+                        <div className="mt-4">
+                          {watchedMasterResume === "masterResume" && (
+                            <CheckboxField
+                              fieldLabel="Use current master resume"
+                              fieldName={`groups.${index}.masterResume`}
+                              control={form.control}
+                            />
+                          )}
+
+                          {watchedMasterResume === "customText" && (
+                            <TextArea
+                              fieldLabel="Add a custom description here"
+                              fieldName={`groups.${index}.customText`}
+                              control={form.control}
+                              placeholder="Add a custom description here.."
+                              required
+                            />
+                          )}
+
+                          {watchedMasterResume === "fileUpload" && (
+                            <SingleFileDropBox
+                              fieldLabel="Upload your resume here"
+                              fieldName={`groups.${index}.fileUpload`}
+                              control={form.control}
+                              required
+                              setValue={form.setValue}
+                              setUploading={() => {}}
+                            />
+                          )}
+                        </div>
+                      </div>
+
+                      <TextInput
+                        fieldLabel="Description of the Company"
+                        fieldName={`groups.${index}.companyDescription`}
+                        control={form.control}
+                        placeholder="Describe in few sentences or paste the link of home page"
+                        required
+                      />
+                      <TextInput
+                        fieldLabel="Additional Information (Highlight any recent projects or experience)"
+                        fieldName={`groups.${index}.additionalInformation`}
+                        control={form.control}
+                        placeholder="Describe if any"
+                      />
+                      <TextInput
+                        fieldLabel="Additional Questions Asked 1"
+                        fieldName={`groups.${index}.additionalQuestion1`}
+                        control={form.control}
+                        placeholder="Write / Paste the Q here"
+                      />
+                      <TextInput
+                        fieldLabel="Additional Questions Asked 2"
+                        fieldName={`groups.${index}.additionalQuestion2`}
+                        control={form.control}
+                        placeholder="Write / Paste the Q here"
+                      />
+                      <TextInput
+                        fieldLabel="Additional Questions Asked 3"
+                        fieldName={`groups.${index}.additionalQuestion3`}
+                        control={form.control}
+                        placeholder="Write / Paste the Q here"
+                      />
+                      <div className="flex items-center pt-4">
+                        <CheckboxField
+                          fieldLabel="Cover Letter Needed"
+                          fieldName={`groups.${index}.coverLetter`}
+                          control={form.control}
+                        />
+                      </div>
+
+                      <div className="col-span-4 mb-4">
+                        <div className="flex items-center justify-end w-full gap-4">
+                          <Button
+                            type="submit"
+                            size="sm"
+                            className="bg-honoluluBlue hover:bg-federalBlue text-white hover:text-white border-federalBlue"
+                            onClick={() => onSectionSubmit(index)}
+                          >
+                            <Send className="size-4 mr-2" />
+                            Generate
+                          </Button>
+
+                          {index === fields.length - 1 && (
+                            <Button
+                              type="button"
+                              onClick={() =>
+                                append({
+                                  jobDescription: "",
+                                  companyDescription: "",
+                                  customInput: "masterResume",
+                                  masterResume: false,
+                                  fileUpload: "",
+                                  customText: "",
+                                  additionalInformation: "",
+                                  additionalQuestion1: "",
+                                  additionalQuestion2: "",
+                                  additionalQuestion3: "",
+                                  coverLetter: false,
+                                })
+                              }
+                              className="bg-blue-700 hover:bg-blue-800 text-white hover:text-white border-blue-700"
+                              size="sm"
+                              variant={"outline"}
+                            >
+                              <PlusCircle className="size-4 mr-2" />
+                              Add
+                            </Button>
+                          )}
+
+                          {fields.length > 1 && (
+                            <Button
+                              type="button"
+                              onClick={() => {
+                                handleDelete(index);
+                              }}
+                              className="bg-red-500 text-white hover:bg-red-600 border-red-600 hover:text-white"
+                              size="sm"
+                              variant={"outline"}
+                            >
+                              <Trash2 className="size-4 mr-2" />
+                              Remove
+                            </Button>
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            );
-          })}
-        </form>
-      </Form>
-    </div>
+                );
+              })}
+            </form>
+          </Form>
+        </>
+      )}
+    </>
   );
 };
