199d6ad9df932323961ebb0616088388e47caadf
Chandima Rathnayake <59339732+Chandima-R@users.noreply.github.com>
2024-11-09 21:55:13 +0530

    feat: update sign in screen

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,151 +1,150 @@
-import { FlipCard } from "@/modules/shared/components/flip-card";
-import { SessionCounterCard } from "@/modules/shared/components/session-counter-card";
-import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
-import {
-  Briefcase,
-  CheckCircle,
-  Loader2,
-  MessageCircle,
-  Search,
-  UserPlus,
-} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function page() {
   return (
     <div className="w-full h-auto">
-      <div
-        className={
-          "w-full max-w-screen mx-auto flex justify-center scroll-auto bg-white border-b"
-        }
-      >
-        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center py-28 px-14 lg:px-16 mx-auto bg-gradient-to-r from-nonPhotoBlue via-lightCyan to-white rounded-lg ">
-          <div className="flex items-center justify-center mb-8">
-            <Briefcase size={40} className="text-honoluluBlue mr-4" />
-            <h1 className="font-extrabold text-2xl lg:text-4xl text-honoluluBlue">
-              Join as a Recruiter
-            </h1>
-          </div>
+      <header className="flex flex-col mx-auto p-4 w-[1280px] max-w-full text-center px-8 h-screen justify-center -mt-16">
+        <h1 className="font-bold text-4xl lg:text-6xl mb-16">
+          Unlock Your Career Potential with an <br />
+          <span className="text-honoluluBlue">ATS-Optimized</span> Resume
+        </h1>
+        <p className="text-lg lg:text-2xl font-medium text-gray-700  mx-auto mb-8">
+          Stand out in today’s competitive job market with a resume designed to
+          pass Applicant Tracking Systems and impress hiring managers. Our
+          powerful resume generator helps you highlight your skills, showcase
+          your experience, and optimize keywords, giving you the edge needed to
+          secure more interviews and land your dream job faster.
+        </p>
 
-          <p className="text-lg lg:text-xl text-center text-slate-600 mb-6">
-            If you are a recruiter, talent sourcer, or employer, we invite you
-            to complete the early registration form below. Join our community
-            and access the best talent!
+        <div>
+          <Button
+            size={"lg"}
+            className="rounded-full py-2 bg-honoluluBlue hover:bg-federalBlue text-white "
+            variant="default"
+          >
+            Sign Up - It&rsquo; Free!
+          </Button>
+
+          <p className="text-lg font-normal text-gray-700 mt-4">
+            7 out of 10 users land their dream job faster with Magic Apply –
+            your shortcut to success!
           </p>
-
-          <div className="flex flex-col space-y-4 mb-8">
-            <div className="flex items-center space-x-3">
-              <CheckCircle size={24} className="text-blue-500" />
-              <span className="text-lg text-slate-700 font-medium">
-                Access a pool of highly skilled professionals.
-              </span>
-            </div>
-            <div className="flex items-center space-x-3">
-              <MessageCircle size={24} className="text-purple-500" />
-              <span className="text-lg text-slate-700 font-medium">
-                Engage with top talent through seamless communication.
-              </span>
-            </div>
-            <div className="flex items-center space-x-3">
-              <Search size={24} className="text-teal-500" />
-              <span className="text-lg text-slate-700 font-medium">
-                Efficiently search and source candidates that match your needs.
-              </span>
-            </div>
-          </div>
-
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center justify-center bg-honoluluBlue text-white px-6 py-3 rounded-md cursor-pointer hover:bg-federalBlue transition-all duration-200 shadow-lg mb-6">
-              <UserPlus size={24} className="mr-2" />
-              <span className="text-md font-medium">Register Now</span>
-            </div>
-          </div>
         </div>
+      </header>
 
-        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center py-28 px-14 lg:px-16 mx-auto bg-white rounded-lg border-l">
-          <h1 className={"font-bold text-4xl lg:text-6xl mb-12 text-slate-700"}>
-            MagicApply
-          </h1>
+      <section className="flex items-centes w-full max-w-[1280px] mx-auto -mt-28">
+        <Tabs defaultValue="resume" className="w-full mx-auto">
+          <TabsList className="flex justify-between p-8 border bg-gray-50">
+            <TabsTrigger
+              value="resume"
+              className="flex items-center text-lg font-medium space-x-2"
+            >
+              🚀 <span>Resume Enhancements with AI</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="apply-jobs"
+              className="flex items-center text-lg font-medium space-x-2"
+            >
+              💼 <span>Apply 50 Jobs at Once</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="comparison"
+              className="flex items-center text-lg font-medium space-x-2"
+            >
+              🔍 <span>Compare Resume to Job</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="cover-letter"
+              className="flex items-center text-lg font-medium space-x-2"
+            >
+              🖹 <span>Generate Specific Cover Letters</span>
+            </TabsTrigger>
+          </TabsList>
 
-          <ClerkLoaded>
-            <SignIn path="/sign-in" />
-          </ClerkLoaded>
-          <ClerkLoading>
-            <Loader2 className="animate-spin text-muted-foreground" />
-          </ClerkLoading>
-        </div>
-      </div>
-
-      <div className="bg-slate-50 overflow-hidden flex justify-center w-full mx-auto">
-        <div className="lg:flex w-full lg:w-2/3 flex-col items-center justify-center py-4 px-10">
-          <div className={"mb-4 w-full"}>
-            <div className={"mb-4"}>
-              <p
-                className={"font-bold text-4xl mb-2 text-slate-700 text-center"}
-              >
-                Important Metrics
+          <TabsContent value="resume">
+            <div className="space-y-4">
+              <h2 className="text-2xl font-bold">
+                Enhance Your Resume with AI
+              </h2>
+              <p className="text-lg text-gray-700">
+                Use cutting-edge AI technology to optimize your resume. Our tool
+                analyzes your experience and ensures your resume is
+                keyword-rich, ATS-friendly, and tailored for the role you want.
+                Let the AI help you craft a standout document in minutes!
               </p>
+              <Image
+                src="/images/dashboard-banner.png"
+                alt="AI Resume Enhancement"
+                className="w-full h-auto rounded-lg"
+                width={1920}
+                height={1080}
+              />
             </div>
-          </div>
+          </TabsContent>
 
-          <div className="flex items-center justify-between gap-4 space-y-4 h-auto py-auto w-full flex-grow">
-            <SessionCounterCard
-              title={"Total Users(this is annoying to update)"}
-              count={"2,442,215"}
-            />
-            <SessionCounterCard title={"Interview Rate"} count={"62.18%"} />
-            <SessionCounterCard title={"Avg. User Review"} count={"8.23/10"} />
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-slate-50 overflow-hidden flex justify-center w-full mx-auto">
-        <div className="lg:flex w-full flex-col items-center justify-center py-4 px-10">
-          <div className={"mb-4 w-full"}>
-            <div className={"mb-4"}>
-              <p
-                className={"font-bold text-4xl mb-2 text-slate-700 text-center"}
-              >
-                Land the Perfect Job Faster with AI-Optimized Resumes!
+          <TabsContent value="apply-jobs">
+            <div className="space-y-4">
+              <h2 className="text-2xl font-bold">Apply to 50 Jobs at Once</h2>
+              <p className="text-lg text-gray-700">
+                Save time and increase your chances by applying to multiple jobs
+                with a single click. Our tool automatically tailors your resume
+                to fit different job descriptions, making it easy for you to
+                reach out to recruiters and hiring managers faster than ever.
               </p>
+              <Image
+                src="/images/dashboard-banner.png"
+                alt="AI Resume Enhancement"
+                className="w-full h-auto rounded-lg"
+                width={1920}
+                height={1080}
+              />
             </div>
-            <p className={"text-slate-500 mb-4 text-lg"}>
-              Magic Apply enhances your resume and cover letter to match 90% of
-              ny job description, so you can apply with confidence.
-            </p>
-          </div>
+          </TabsContent>
 
-          <div className="flex items-center justify-between  gap-4 h-auto w-full">
-            <FlipCard
-              frontTitle="AI-Powered Tailoring"
-              frontDescription=" Our cutting-edge I analyzes and tailors your resume for the best
-            fit."
-              backTitle="Intelligent Matching"
-              backDescription="We optimize your application to match job descriptions with 90%
-            accuracy."
-            />
-            <FlipCard
-              frontTitle="One-Click Application"
-              frontDescription="Apply for multiple jobs in a single go, saving hours of time."
-              backTitle="Resume Perfection"
-              backDescription="Get resume feedback and enhancements powered by AI, tailored to specific industries."
-            />
-
-            <FlipCard
-              frontTitle="Job Match Fuarantee"
-              frontDescription="Magic Apply ensures a 90%+ match with job descriptions to increase your interview chances."
-              backTitle="Effortless Application"
-              backDescription="Submit 50 applicaitons with one click, no more manual entry."
-            />
-          </div>
-
-          <div className="w-full flex items-center justify-center my-8">
-            <div className="w-auto flex items-center justify-center bg-white border-federalBlue border-2 text-black px-12 py-3 rounded-md cursor-pointer hover:bg-federalBlue hover:text-white transition-all duration-200 shadow-lg mb-6">
-              <span className="text-md font-medium">Get started for free</span>
+          <TabsContent value="comparison">
+            <div className="space-y-4">
+              <h2 className="text-2xl font-bold">
+                Compare Your Resume to Job Descriptions
+              </h2>
+              <p className="text-lg text-gray-700">
+                Ensure your resume aligns perfectly with the job description.
+                Our comparison tool checks for keyword matches and highlights
+                areas for improvement, giving you the best chance of getting
+                noticed by hiring managers.
+              </p>
+              <Image
+                src="/images/dashboard-banner.png"
+                alt="AI Resume Enhancement"
+                className="w-full h-auto rounded-lg"
+                width={1920}
+                height={1080}
+              />
             </div>
-          </div>
-        </div>
-      </div>
+          </TabsContent>
+
+          <TabsContent value="cover-letter">
+            <div className="space-y-4">
+              <h2 className="text-2xl font-bold">
+                Generate Customized Cover Letters
+              </h2>
+              <p className="text-lg text-gray-700">
+                Quickly create personalized cover letters for each job you apply
+                to. Our generator tailors each letter based on the job
+                description, ensuring it’s professional, unique, and compelling.
+              </p>
+              <Image
+                src="/images/dashboard-banner.png"
+                alt="AI Resume Enhancement"
+                className="w-full h-auto rounded-lg"
+                width={1920}
+                height={1080}
+              />
+            </div>
+          </TabsContent>
+        </Tabs>
+      </section>
     </div>
   );
 }
